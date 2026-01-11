881c5a8ee8d24e3214fd57f824c43be049428e29
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-11 17:07:06 +0200

    chore: add MongoDB connection with mongoose cache

---
+++ src/lib/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) throw new Error("MongoDB URI doesn`t exist!");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export default async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(mongoURI).then((mongoose) => {
+      console.log("✅ MongoDB connected:", mongoose.connection.name);
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
