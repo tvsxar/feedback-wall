4577e7f865f081a48fce402ba00e7b06a2e6a31a
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-11 22:21:08 +0200

    fix: replaced date property with createdAt

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,12 +1,12 @@
 import formatDate from "@/lib/utils";
 
 export default function FeedbackCard({ feedback }) {
-    const avatarLetter = feedback.name?.[0]?.toUpperCase() || "A";
+  const avatarLetter = feedback.name?.[0]?.toUpperCase() || "A";
 
   return (
     <li
       className="flex flex-col gap-4 border border-gray-600 
-      rounded-lg p-6 w-full max-w-120 bg-gray-800 
+      rounded-lg p-6 w-full bg-gray-800 
       hover:scale-101 transition-all duration-300"
     >
       <div className="flex justify-between items-center">
@@ -21,7 +21,7 @@ export default function FeedbackCard({ feedback }) {
         </div>
 
         <div
-  className={`py-1 px-3 rounded-full text-sm font-medium transition-all duration-200 cursor-default
+          className={`py-1 px-3 rounded-full text-sm font-medium transition-all duration-200 cursor-default
   ${
     feedback.type === "Idea"
       ? "bg-linear-to-r from-violet-400/40 to-violet-500/30 text-violet-300"
@@ -29,9 +29,9 @@ export default function FeedbackCard({ feedback }) {
       ? "bg-linear-to-r from-pink-400/40 to-pink-500/30 text-pink-300"
       : "bg-linear-to-r from-blue-400/40 to-blue-500/30 text-blue-300"
   }`}
->
-  {feedback.type}
-</div>
+        >
+          {feedback.type}
+        </div>
       </div>
 
       <div className="bg-gray-800/50 rounded-lg py-2">
@@ -39,7 +39,9 @@ export default function FeedbackCard({ feedback }) {
       </div>
 
       <div className="flex justify-end">
-        <p className="text-sm text-gray-500">{formatDate(feedback.date)}</p>
+        <p className="text-sm text-gray-500">
+          {formatDate(feedback.createdAt)}
+        </p>
       </div>
     </li>
   );
