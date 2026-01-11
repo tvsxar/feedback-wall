f5fa34466fa9654cf634b8e2fd047de98a554704
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-11 22:38:21 +0200

    refactor: add client-side data fetching, improve header section and handle loading, error and empty states

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,23 +1,46 @@
+"use client";
 import FeedbacksList from "@/components/FeedbacksList";
+import { useEffect } from "react";
+import useFeedback from "@/lib/hooks/useFeedback";
+import LoadingOverlay from "@/components/LoadingOverlay";
+import ErrorOverlay from "@/components/ErrorOverlay";
+import EmptyState from "@/components/EmptyState";
 
 export default function FeedbacksPage() {
+  const { feedbacks, error, loading, fetchFeedbacks } = useFeedback();
+
+  useEffect(() => {
+    fetchFeedbacks();
+  }, []);
+
   return (
     <div className="p-4">
-      <div className="flex flex-col items-center text-center mb-12">
+      {loading && <LoadingOverlay message="Fetching feedbacks..." />}
+      {error && <ErrorOverlay message={error} onRetry={fetchFeedbacks} />}
+
+      <div className="max-w-4xl mx-auto text-center mb-16">
         <h1
-          className="text-4xl md:text-5xl font-extrabold tracking-tight 
-      bg-linear-to-b from-white via-gray-300 to-gray-400
-      bg-clip-text text-transparent drop-shadow-[0_0_8px_rgba(255,255,255,0.3)]"
+          className="text-4xl md:text-5xl font-extrabold tracking-tight
+          bg-linear-to-b from-white via-gray-300 to-gray-400
+          bg-clip-text text-transparent"
         >
-          Read what people think about the service
+          Community Feedback
         </h1>
         <p className="text-gray-400 text-lg mt-4">
-          Honest feedback from the community
+          Honest thoughts from real users
         </p>
       </div>
 
-      <div className="flex flex-col items-center">
-        <FeedbacksList />
+      <div className="max-w-6xl mx-auto">
+        {loading && <LoadingOverlay message="Loading feedbacks..." />}
+
+        {error && <ErrorOverlay message={error} onRetry={fetchFeedbacks} />}
+
+        {!loading && !error && feedbacks.length === 0 && <EmptyState />}
+
+        {!loading && !error && feedbacks.length > 0 && (
+          <FeedbacksList feedbacks={feedbacks} />
+        )}
       </div>
     </div>
   );
