a98fc0748abbce027001743f860035f9290f5aed
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-12 13:45:15 +0200

    fix: fixed bg color of layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-gray-900 text-gray-100 ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-gray-950 text-gray-100 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
